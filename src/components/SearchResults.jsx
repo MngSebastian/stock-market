7416fc3885c2531cb635f0d76b4aa8e7003d46b3
Sebastian Mosneagu <44436863+MngSebastian@users.noreply.github.com>
2023-09-27 09:03:28 +0200

    Styled SearchResults for Dark/Light mode and added border to each element in search

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
+import ThemeContext from "../context/ThemeContext";
+
 // add: when no stock ticker matches the input
 // open the dropdown with "No stocks found wit this ticker."
 function SearchResults({
@@ -7,15 +9,21 @@ function SearchResults({
   setBestMatches,
   setInputValue,
 }) {
+  const { lightMode } = useContext(ThemeContext);
+
   return (
     <ul
-      className={`absolute top-32 left-4 bg-primary border-2 custom-scrollbar border-slate-500 w-[300px] md:w-[400px] z-40 rounded-md h-64 overflow-y-scroll`}
+      className={`absolute top-32 left-4 ${
+        lightMode ? "bg-white text-black" : "bg-primary "
+      } border-2 custom-scrollbar border-slate-500 w-[300px] md:w-[400px] z-40 rounded-md h-64 overflow-y-scroll`}
     >
       {results.map((item) => {
         return (
           <li
             key={item.symbol}
-            className={`cursor-pointer p-4 m-2  hover:bg-pink-500 flex items-center justify-between rounded-md transition duration-300`}
+            className={`cursor-pointer ${
+              lightMode ? "shadow-light" : "shadow-dark"
+            } p-4 m-2  hover:bg-pink-500 flex items-center justify-between rounded-md transition duration-300`}
             onClick={() => {
               setCompanySymbol(item.symbol);
               setInputValue("");
